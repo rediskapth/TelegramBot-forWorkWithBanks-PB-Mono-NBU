1072b460df8d5dfd545c3442afa5862961c254ad
ognie <skaymorik@gmail.com>
2021-12-20 16:31:12 +0200

    config Bot 1.4.2

--- telegramBot/src/main/java/api/config/TelegramConfig.java
+++ telegramBot/src/main/java/api/config/TelegramConfig.java
@@ -3,4 +3,5 @@ package api.config;
 public class TelegramConfig {
     public static final String BOT_NAME = "";
     public static final String BOT_TOKEN= "";
+    public static final String bot= " ";
 }
