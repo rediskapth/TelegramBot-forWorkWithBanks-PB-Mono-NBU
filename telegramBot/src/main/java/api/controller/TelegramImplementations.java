899b38407f285bb0a5ad4c5e75590453f4bafdba
Hrabr <emecandrei83@gmail.com>
2021-12-20 17:38:43 +0200

    Add Frame

--- telegramBot/src/main/java/api/controller/TelegramImplementations.java
+++ telegramBot/src/main/java/api/controller/TelegramImplementations.java
@@ -1,4 +1,73 @@
 package api.controller;
 
-public class TelegramImplementations {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import utils.keyboards.InlineKeyboardMarkupMy;
+
+import static api.config.TelegramConfig.BOT_NAME;
+import static api.config.TelegramConfig.BOT_TOKEN;
+
+public class TelegramImplementations extends TelegramLongPollingBot {
+
+
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    public void onUpdateReceived(Update update) {
+        if (update.hasCallbackQuery()) {
+            pressingTheButton(update);
+
+
+        } else if (update.hasMessage()) {
+
+            messageHandler(update);
+        }
+    }
+
+
+    private void messageHandler(Update update) {
+        InlineKeyboardMarkupMy inlineKeyboardMarkupMy =new InlineKeyboardMarkupMy();
+        if (update.getMessage().hasText()) {
+
+            String text = update.getMessage().getText();
+            String chatUserId = update.getMessage().getChatId().toString();
+
+            if (text.equals("/start")) {
+                inlineKeyboardMarkupMy .mainMenu(chatUserId);
+            }
+        }
+    }
+
+    private void pressingTheButton(Update update) {
+        InlineKeyboardMarkupMy inlineKeyboardMarkupMy =new InlineKeyboardMarkupMy();
+        String data = update.getCallbackQuery().getData();
+        String chatUserId = update.getCallbackQuery().getMessage().getChatId().toString();
+        switch (data) {
+            case "Settings" ->inlineKeyboardMarkupMy.menuSettings(chatUserId);
+            case "Back" -> inlineKeyboardMarkupMy.mainMenu(chatUserId);
+            case "Number" -> inlineKeyboardMarkupMy.menuNumber(chatUserId);
+            case "BackNum" -> inlineKeyboardMarkupMy.menuSettings(chatUserId);
+            case "Bank" -> inlineKeyboardMarkupMy.menuBanks(chatUserId);
+            case "BackB" -> inlineKeyboardMarkupMy.menuSettings(chatUserId);
+            case "currencies" -> inlineKeyboardMarkupMy.menuCurrency(chatUserId);
+            case "BackVal" -> inlineKeyboardMarkupMy.menuSettings(chatUserId);
+        }
+    }
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new TelegramImplementations());
+        } catch (TelegramApiException e) {
+            e.getStackTrace();
+        }
+    }
+
 }
