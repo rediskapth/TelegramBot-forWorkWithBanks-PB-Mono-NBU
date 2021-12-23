f3f0409a0be8ada97e2a6d885fd82d73dbf82e8c
PaulTy <paultych@gmail.com>
2021-12-23 19:26:01 +0200

    Add User service with load and save to file and get and put/replace methods Add GSON to pom.xml

--- telegramBot/src/main/java/utils/user/UserService.java
+++ telegramBot/src/main/java/utils/user/UserService.java
@@ -1,4 +1,59 @@
 package utils.user;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class UserService {
+    private static Map<Long, UserSettings> userList;
+    private static final String USERDATA_FILE_NAME = "users.json";
+
+    private UserService() {
+        userList = new ConcurrentHashMap<>();
+    }
+
+    //Get element of Map with user settings
+    public UserSettings getUserSettings(Long userId){
+        return userList.get(userId);
+    }
+
+    //Add or change Map element with user settings
+    public void setUserSettings(Long userId, UserSettings userSettings){
+        if (userList.containsKey(userId)){
+            userList.replace(userId, userSettings);
+        } else {
+            userList.put(userId, userSettings);
+        }
+    }
+
+    public static void readUsersFromFile() {
+        File usersFile = new File(USERDATA_FILE_NAME);
+        String line;
+        try (BufferedReader reader = new BufferedReader(new FileReader(usersFile))) {
+            userList = new Gson()
+                    .fromJson(reader, new TypeToken<HashMap<String, UserSettings>>() {
+                    }.getType());
+            System.out.println(userList);//for check
+        } catch (IOException e) {
+            System.out.println(e.getMessage());  //it may not the best way
+        }
+    }
+
+    public static void saveUsersToFile() {
+        Gson json = new GsonBuilder().setPrettyPrinting().create();
+        File file = new File(USERDATA_FILE_NAME);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            gson.toJson(userList, writer);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
+
