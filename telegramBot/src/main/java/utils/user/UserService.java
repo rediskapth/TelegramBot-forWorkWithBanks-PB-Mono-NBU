f855cd0f2396b99cab0143235104026bf2405a17
PaulTy <paultych@gmail.com>
2021-12-27 15:43:10 +0200

    cemmented saveToFile

--- telegramBot/src/main/java/utils/user/UserService.java
+++ telegramBot/src/main/java/utils/user/UserService.java
@@ -15,63 +15,69 @@ import java.util.concurrent.ConcurrentMap;
 public class UserService {
     private ConcurrentMap<Long, UserSettings> userList;
     private static final String USERDATA_FILE_NAME = "users.json";
+    private static File file = new File(USERDATA_FILE_NAME);
+    private static UserService serviceInstance;
 
     public UserService() {
         userList = new ConcurrentHashMap<>();
         readUsersFromFile();
     }
 
-    private static final UserSettings defaultSettings = new UserSettings("NoName",
-            new HashSet<Banks> (Arrays.asList(Banks.PRIVATBANK)),
-            new HashSet<CurrencyNames>(Arrays.asList(CurrencyNames.USD)), 2, 9);
+    public static UserService getInstance() {
+        return Objects.requireNonNullElseGet(serviceInstance, UserService::new);
+    }
 
-    public Boolean isUserExists(Long userId){
+    private static final UserSettings defaultSettings = new UserSettings("NoName",
+            new HashSet<>(List.of(Banks.PRIVATBANK)),
+            new HashSet<>(List.of(CurrencyNames.USD)), 2, 9);
+
+    public Boolean isUserExists(Long userId) {
         return userList.containsKey(userId);
     }
 
     //Записываем банк в настройки пользователя, если его нет, то добавляется/если есть то не дублируется
-    public void setBank (Long userId, String bank){
+    public void setBank(Long userId, String bank) {
         HashSet<Banks> banks = userList.get(userId).getBankList();
         banks.add(Banks.valueOf(bank));
         userList.get(userId).setBankList(banks);
-        saveUsersToFile();
+        //saveUsersToFile();
     }
 
     //Удаляем банк из настройки пользователя
-    public void unSetBank (Long userId, String bank){
+    public void unSetBank(Long userId, String bank) {
         HashSet<Banks> banks = userList.get(userId).getBankList();
         banks.remove(Banks.valueOf(bank));
         userList.get(userId).setBankList(banks);
-        saveUsersToFile();
+        //saveUsersToFile();
     }
 
     //Записываем валюту в настройки пользователя, если ее нет, то добавляется/если есть то не дублируется
-    public void setCurrency (Long userId, String currency){
+    public void setCurrency(Long userId, String currency) {
         HashSet<CurrencyNames> currencies = userList.get(userId).getCurrencies();
         currencies.add(CurrencyNames.valueOf(currency));
         userList.get(userId).setCurrencies(currencies);
-        saveUsersToFile();
+        //saveUsersToFile();
     }
 
     //Удаляем валюту из настроек пользователя
-    public void unSetCurrency (Long userId, String currency){
+    public void unSetCurrency(Long userId, String currency) {
         HashSet<CurrencyNames> currencies = userList.get(userId).getCurrencies();
         currencies.remove(CurrencyNames.valueOf(currency));
         userList.get(userId).setCurrencies(currencies);
-        System.out.println(userList.get(userId).getCurrencies().toString());
-        saveUsersToFile();
+      //  System.out.println(userList.get(userId).getCurrencies().toString());
+        //saveUsersToFile();
     }
 
     //Записываем кол-во знаков после запятой в пользователя
-    public void  setAccuracy(Long userId, int accuracy){
+    public void setAccuracy(Long userId, int accuracy) {
         userList.get(userId).setRoundAccuracy(accuracy);
-        saveUsersToFile();
+        //saveUsersToFile();
     }
 
     //Записываем время оповещения 0- не оповещать
-    public void  setNotify(Long userId, int notifyHour){
+    public void setNotify(Long userId, int notifyHour) {
         userList.get(userId).setNotifyHour(notifyHour);
-        saveUsersToFile();
+        //saveUsersToFile();
     }
 
 
@@ -94,7 +100,7 @@ public class UserService {
         saveUsersToFile();
     }
 
-    public void readUsersFromFile() {
+    private void readUsersFromFile() {
         File usersFile = new File(USERDATA_FILE_NAME);
         String line;
         try (BufferedReader reader = new BufferedReader(new FileReader(usersFile))) {
@@ -109,8 +115,6 @@ public class UserService {
 
     public void saveUsersToFile() {
         Gson json = new GsonBuilder().setPrettyPrinting().create();
-        File file = new File(USERDATA_FILE_NAME);
-
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             gson.toJson(userList, writer);
